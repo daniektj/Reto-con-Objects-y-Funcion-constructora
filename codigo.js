ddca1c739b41226458a57837a8c2381effb8b1b7
DaniekTj <65110807+daniektj@users.noreply.github.com>
2020-05-30 02:44:19 -0700

    corrección
    Se reparo el error en la captura del valor seleccionado por el usuario.

--- codigo.js
+++ codigo.js
@@ -13,9 +13,11 @@ function autos_registro(marca, modelo, ano){
 }
 
 
-var cantidad_registros = document.getElementById("myText").value/3;
+
 
 function capturar(){
+    var cantidad_registros = document.getElementById("myText").value;
+    console.log(cantidad_registros);
 
 
     for (var i = 0; i <= cantidad_registros; ++i) {
@@ -30,45 +32,7 @@ function capturar(){
         this["nuevo_registro"+i] = new autos_registro("Dodge", modelos_Ford[Math.floor(Math.random() * 3)], anos[Math.floor(Math.random() * 4)]);
         document.getElementById("tabla").innerHTML += "<td>" + this["nuevo_registro"+i].marca + "</td>"+"<td>" + this["nuevo_registro"+i].modelo + "</td>"+"<td>" + this["nuevo_registro"+i].ano + "</td>";
         baseDatos.push(this["nuevo_registro"+i]);
+        i++
     }
 
-
-    console.log("botonazo");
 }
-
-
-
-
-//document.getElementById("tabla").innerHTML += "<td>" + nuevo_registro.marca + "</td>";
-
-
-
-/* var obj_to_str =  
-{ name: "GeeksForGeeks", city: "Noida", contact:2488 }; 
-var myJSON = JSON.stringify(obj_to_str); 
-document.write(myJSON)  */
-
-
-/* function capturar() {
-  var x = document.getElementById("myText").value;
-  document.getElementById("demo").innerHTML = x;
-} */
-
-
-
-/* function agregar(){
-    baseDatos.push(nuevo_registro);
-    console.log(baseDatos);
-} */
-
-
-
-
-
-
-/*     for (var i = 0; i <= 8; ++i) {
-        this["marker"+i] = "add here";
-     }
-    
-     var a = this.marker0;
-    alert(a); */
